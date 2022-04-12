d90e0ee8ec6bc63439dea9985fc307ad6490db52
Nicolas Grossi <nicolas.grossi@gmail.com>
2022-04-12 17:42:49 -0300

    initial commit

---
+++ example.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto('http://www.google.com');
+  await page.waitFor(1000)
+
+  // Get the "viewport" of the page, as reported by the page.
+  const dimensions = await page.evaluate(() => {
+    return {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+      deviceScaleFactor: window.devicePixelRatio,
+    };
+  });
+
+  console.log('Dimensions:', dimensions);
+
+  await browser.close();
+})();
