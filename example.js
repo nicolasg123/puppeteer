91b39f267d654674689d1219a0c27b4c98c72c87
Nicolas Grossi <nicolas.grossi@gmail.com>
2022-04-12 19:48:31 -0300

    plying with puppeter

--- example.js
+++ example.js
@@ -3,19 +3,22 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('http://www.google.com');
-  await page.waitFor(1000)
+  await page.goto('https://www.atlassian.com/');
+  await page.waitFor(2000)
 
   // Get the "viewport" of the page, as reported by the page.
-  const dimensions = await page.evaluate(() => {
-    return {
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight,
-      deviceScaleFactor: window.devicePixelRatio,
-    };
-  });
+//   const dimensions = await page.evaluate(() => {
+//     return {
+//       width: document.documentElement.clientWidth,
+//       height: document.documentElement.clientHeight,
+//       deviceScaleFactor: window.devicePixelRatio,
+//     };
+//   });
 
-  console.log('Dimensions:', dimensions);
-
+//   console.log('Dimensions:', dimensions);
+  await page.click('a.global-nav--wac__button--primary')
+  await page.waitFor(2000)
+  await page.click('a.global-nav--wac__button--secondary')
+  await page.waitFor(2000)
   await browser.close();
 })();
